6664b8ee441e8891388db60f3708ae882adb4a60
wojbiel833 <wojbiel833@gmail.com>
2021-07-29 16:44:06 +0200

    Add first tasks

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const express = require("express");
+const socket = require("socket.io");
+
+const tasks = [];
 
 const app = express();
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log("Server is running...");
 });
+const io = socket(server);
 
 app.use((req, res) => {
   res.status(404).send({ message: "Not found..." });
 });
+
+io.on("connction", (socket) => {
+  socket.to(socket.id).emit("updateData", tasks);
+
+  io.on("addTask", (taskName) => {
+    tasks.push(taskName);
+    socket.broadcast.emit("addTask", taskName);
+  });
+
+  io.on("removeTask", (taskName) => {
+    const index = tasks.findIndex(taskName);
+    tasks.splice(taskName[index], 1);
+    socket.broadcast.emit("removeTask", taskName);
+  });
+});
+
+// const user = users.find((item) => item.id === socket.id);
+//     const index = users.findIndex((item) => item.id === socket.id);
+//     tasks.splice(index, 1);
+// socket.broadcast.emit("removeUser", nick);
