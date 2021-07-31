96795069afcd696c27ad37b1a8691b5078c544bb
wojbiel833 <wojbiel833@gmail.com>
2021-07-31 15:23:50 +0200

    Add some functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,27 @@
 import React from "react";
+import io from "socket.io";
 
 class App extends React.Component {
+  state = {
+    tasks: ["Shopping"],
+  };
+
+  componentDidMount() {
+    this.socket = io();
+    // this.socket.connect("http://localhost:8000");
+    //CO JEST NIE TAK Z POLACZENIEM HOSTOW?
+  }
+
+  removeTask(id) {
+    console.log("remove");
+    // const task = this.state.tasks.find((item) => item.id === id);
+    const index = this.state.tasks.findIndex(
+      (item) => item.id === this.socket.id
+    );
+
+    this.state.tasks.splice(index, 1);
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,18 +33,20 @@ class App extends React.Component {
           <h2>Tasks</h2>
 
           <ul className="tasks-section__list" id="tasks-list">
-            <li class="task">
-              Shopping <button class="btn btn--red">Remove</button>
-            </li>
-            <li class="task">
-              Go out with a dog <button class="btn btn--red">Remove</button>
-            </li>
+            {this.state.tasks.map((task) => (
+              <li key={task + task.length} className="task">
+                {task}
+                <button className="btn btn--red" onClick={this.removeTask}>
+                  Remove
+                </button>
+              </li>
+            ))}
           </ul>
 
           <form id="add-task-form">
             <input
               className="text-input"
-              autocomplete="off"
+              autoComplete="off"
               type="text"
               placeholder="Type your description"
               id="task-name"
